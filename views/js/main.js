f06f174d76ff69f212891ee2f716e63da9fee76e
Brett Freese <Brett Freese>
2016-02-15 11:01:05 -0500

    Modified phase to remove part of calc from loop.

--- views/js/main.js
+++ views/js/main.js
@@ -504,8 +504,10 @@ function updatePositions() {
 
   var items = document.querySelectorAll('.mover');
   //Moved phase var assignment out of loop so it is hit only once
-  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  //var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  var phase = Math.sin(document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
+    phase = phase + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
