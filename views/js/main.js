d5adfd185e89b2964e55fd71fda836ba515a9a84
Brett Freese <Brett Freese>
2016-02-16 09:46:37 -0500

    Fix name of changePizzaSizes function

--- views/js/main.js
+++ views/js/main.js
@@ -461,7 +461,7 @@ var resizePizzas = function(size) {
   }
   */
 
-  function git (size) {
+  function changePizzaSizes(size) {
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
